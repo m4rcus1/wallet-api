2205ac54c22d2bcd91d476eb3429c1e26e3eb3b3
m4rcus1 <anhq6009@gmail.com>
2023-05-30 14:00:56 +0700

    fix ncors

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ref } = require('./firebase.js');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const cors = require('cors'); // Import the cors module
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Enable CORS for all routes
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-// Read data
-ref.once('value')
-  .then((snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('Error reading data:', error);
-  });
 
-app.get('/', (req, res) => {
-  ref.once('value')
-    .then((snapshot) => {
-      const data = snapshot.val();
-      console.log(data);
-      res.json(data);
-    })
+// Read data
+const readData = () => {
+  return ref
+    .once('value')
+    .then((snapshot) => snapshot.val())
     .catch((error) => {
       console.error('Error reading data:', error);
-      res.status(500).json({ error: 'Error reading data' });
+      throw error;
     });
+};
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await readData();
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error('Error reading data:', error);
+    res.status(500).json({ error: 'Error reading data' });
+  }
 });
 
 app.get('/add', (req, res) => {
@@ -45,7 +47,7 @@ app.get('/add', (req, res) => {
 
 app.post('/add', (req, res) => {
   const { account, password } = req.body;
-  
+
   if (!account || !password) {
     return res.status(400).json({ error: 'Missing account or password in the request body' });
   }
@@ -58,17 +60,5 @@ app.post('/add', (req, res) => {
     },
   };
 
-  ref.update(newData)
-    .then(() => {
-      console.log('Data updated successfully.');
-      res.json(newData);
-    })
-    .catch((error) => {
-      console.error('Error updating data:', error);
-      res.status(500).json({ error: 'Failed to update data' });
-    });
-});
-
-app.listen(port, () => {
-  console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`);
-});
+  ref
+    .update(new
